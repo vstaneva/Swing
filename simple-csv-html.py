b4faa3dab7cb543cf41a1d1a9a8ec65fa82facca
Val Staneva <)>
2014-10-17 13:19:18 -0400

    Initial files I'm working with
    small_example.csv has only the columns that I’m working with right now.
    The initial goal is to have this very little information rendered as a
    web page so I have a “backbone” for rendering the more complicated data
    for a real experiment.
    
    simple-csv-html.py is a script that reads CSV data from the
    small_example file and puts it into a series of HTML documents. Right
    now, it creates the text for one task and displays it on the standard
    output for debugging purposes.
    
    CSV_example_val.csv contains information about what a real life
    experiment could be like.
    
    Page-layout.html describes a very simple HTML page what I currently use
    as a template for the generated HTML pages.

---
+++ simple-csv-html.py
@@ -0,0 +1,54 @@
+import csv
+import random
+
+def parseCSV (path):
+    """Parses .csv experiment file (see documentation for .csv file formatting instructions.)
+       It currently has the following columns: (int) "ITEM", (string) "CONDITION",
+       (string) "FORM TEXT", (string) "QUESTION", (string) "TYPE", (string) "ANSWERS"  """
+    tasklist = []
+    with open(path, 'rU') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            tasklist.append(row)
+    tasklist.remove(tasklist[0])
+    return tasklist
+
+def getRandomItem(tasks):
+    """Tasks is a list of lines from the CSV file. We assume that all of the elements
+       have the same item number.
+       Currently, we assume that there is one condition per item.
+       Be careful here^. In the future we'd like to select by Condition, not by Item.
+       How to do that:
+           1. We could separate tasks by (item,condition) and select a random condition, or
+           2. We could select a random item and execute all the items that have the same condition as it."""
+    return random.choice(tasks)
+    
+def CVStoHTML(tasks):
+    sameItem = []
+    HTMLstuffing = []
+    for row in tasks:
+        if not sameItem:
+            sameItem.append(row)
+        elif row[0]==sameItem[len(sameItem)-1][0]:
+            sameItem.append(row)
+        else:
+            HTMLstuffing.append(getRandomItem(sameItem))
+            sameItem = []
+    return HTMLstuffing
+
+def HTMLfiller(item,stuffing):
+    #for task in stuffing:
+    print "<html>\n<head>\n</head>\n<body>\n"
+    print "<p> Experiment </p>\n"
+    #condition, form text and question
+    print "<p>%s</p>\n<p>%s</p>\n<p>%s</p>\n" % (stuffing[item][0], stuffing[item][1], stuffing[item][2])
+    print "<ul>\n<li> Answer1 </li>\n<li> Answer 2 </li>\n<li> Answer 3 </li>\n</ul>\n"
+    print "</body>\n</html>"
+
+tasklist = parseCSV("/Users/val/Documents/UROP-Fall2015/small_example.csv")
+for task in tasklist:
+    print task
+HTMLstuffing = CVStoHTML(tasklist)
+print HTMLstuffing
+HTMLfiller(0, HTMLstuffing)
+
